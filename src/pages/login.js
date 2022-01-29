0295642e94c97f3997ba19c7ee3c271084826e22
talhajubaer018 <52033279+talhajubaer018@users.noreply.github.com>
2022-01-29 12:17:53 +0600

    login page design

--- src/pages/login.js
+++ src/pages/login.js
@@ -27,21 +27,23 @@ const Login = () => {
   }, [router, userInfo])
 
   return (
-    <div className='w-1/2 mx-auto'>
-      <h2 className='text-center'>LOGIN</h2>
-      <div className='text-center flex flex-col justify-center mb-4'>
-        <label className='text-left'>Phone</label>
-        <input type='number' value={phone} onChange={(e) => setPhone(e.target.value)} className='border-2 p-2 w-full' />
-      </div>
-      <div className='text-center flex flex-col justify-center mb-4'>
-        <label className='text-left'>Password</label>
-        <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='border-2 p-2 w-full' />
-      </div>
-      <div>
-        {error && <div className='text-red-500'>{error}</div>}
-      </div>
-      <div className=' text-center' onClick={loginClick}>
-        <button className='px-12 py-4 rounded-full bg-black text-white'>Login</button>
+    <div className='flex flex-col w-1/2 mx-auto items-center justify-center pt-12'>
+      <div className='w-1/2 mx-auto'>
+        <h2 className='text-center text-white'>Sign In</h2>
+        <div className='text-center flex flex-col justify-center mb-4'>
+          <label className='text-left text-white'>Phone</label>
+          <input type='text' value={phone} onChange={(e) => setPhone(e.target.value)} className='input border-2 border-solid border-offWhite-300 rounded-md p-2 w-full bg-offWhite-50 opacity-50 text-white' />
+        </div>
+        <div className='text-center flex flex-col justify-center mb-4'>
+          <label className='text-left text-white'>Password</label>
+          <input type='password' value={password} onChange={(e) => setPassword(e.target.value)} className='input border-2 border-solid border-offWhite-300 rounded-md p-2 w-full bg-offWhite-50 opacity-50 text-white' />
+        </div>
+        <div>
+          {error && <div className='text-red-500'>{error}</div>}
+        </div>
+        <div className=' text-center' onClick={loginClick}>
+          <button className='w-full px-12 py-4 bg-transparent text-customTeal-500 border-solid border-1 rounded-3xl border-customTeal-500 hover:bg-customTeal-500 hover:text-black transition-all duration-300'>Login</button>
+        </div>
       </div>
     </div>
   );
